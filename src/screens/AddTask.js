16f4d3d30bd22e8c289ae5c34089407abe1cf6af
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-05-18 22:14:48 -0300

    criação do modal para adicionar novas tasks

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -22,4 +22,43 @@ export default class AddTask extends Component {
     this.props.onSave(data);
     this.setState({...initialState});
   };
+
+  render() {
+    return (
+      <Modal
+        onRequestClose={this.props.onCancel}
+        visible={this.props.isVisible}
+        animationType="slide"
+        transparent={true}>
+        <TouchableWithoutFeedback onPress={this.props.onCancel}>
+          <View style={styles.offset}></View>
+        </TouchableWithoutFeedback>
+        <View style={styles.container}>
+          <Text style={styles.header}>Nova Tarefa!</Text>
+          <TextInput
+            placeholder="Descrição..."
+            style={styles.input}
+            onChangeText={(desc) => this.setState({desc})}
+            value={this.state.desc}
+          />
+          <DatePickerIOS
+            mode="date"
+            date={this.state.date}
+            onDateChange={(date) => this.setState({date})}
+          />
+          <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+            <TouchableOpacity onPress={this.props.onCancel}>
+              <Text style={styles.button}>Cancelar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.save}>
+              <Text style={styles.button}>Salvar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <TouchableWithoutFeedback onPress={this.props.onCancel}>
+          <View style={styles.offset}></View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    );
+  }
 }
