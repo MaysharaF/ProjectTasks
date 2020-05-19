450b021fb24a96996b608668fb0e597b80608dc2
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-05-18 22:04:01 -0300

    criação da função para salvar tasks

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -13,3 +13,13 @@ import moment from 'moment';
 import commonStyles from '../commonStyles';
 
 const initialState = {desc: '', date: new Date()};
+
+export default class AddTask extends Component {
+  state = {...initialState};
+
+  save = () => {
+    const data = {...this.state};
+    this.props.onSave(data);
+    this.setState({...initialState});
+  };
+}
