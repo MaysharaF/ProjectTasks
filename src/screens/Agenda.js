38ca50aa1599f4f325763ff590c1093e3db36ba0
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-05-18 21:33:29 -0300

    criação da função toggleTask para check da tarefa

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -84,6 +84,17 @@ export default class Agenda extends Component {
     ],
   };
 
+  toggleTask = (id) => {
+    const tasks = this.state.tasks.map((task) => {
+      if (task.id === id) {
+        task = {...task};
+        task.doneAt = task.doneAt ? null : new Date();
+      }
+      return task;
+    });
+    this.setState({tasks});
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -99,7 +110,9 @@ export default class Agenda extends Component {
           <FlatList
             data={this.state.tasks}
             keyExtractor={(item) => `${item.id}`}
-            renderItem={({item}) => <Task {...item} />}
+            renderItem={({item}) => (
+              <Task {...item} toggleTask={this.toggleTask} />
+            )}
           />
         </View>
       </View>
