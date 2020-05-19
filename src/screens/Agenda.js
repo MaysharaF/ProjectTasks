59eef4158d363dbce54606c13ace064024167d43
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-05-18 21:09:21 -0300

    componente agenda definição e aplicação de estilos

---
+++ src/screens/Agenda.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import todayImagem from '../../assets/imgs/today.jpg';
+import commonStyles from '../commonStyles';
+import Task from '../components/Tasks';
+
+export default class Agenda extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={todayImagem} style={styles.background}>
+          <View style={styles.titleBar}>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>
+              {moment().locale('pt-br').format('ddd, D [de] MMMM')}
+            </Text>
+          </View>
+        </ImageBackground>
+        <View style={styles.taskContainer}>
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Tarefa Pendente" estimateAt={new Date()} doneAt={null} />
+          <Task
+            desc="Tarefa Concluída"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 3,
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
+  },
+  taskContainer: {
+    flex: 7,
+  },
+});
