636edfb234c60bec9529c72379d95d70f411f05b
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-05-29 18:46:07 -0300

    definindo estilo/render da tela de autenticação

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -27,4 +27,63 @@ export default class Auth extends Component {
       Alert.alert('Sucesso!', Logar);
     }
   };
+
+  render() {
+    return (
+      <ImageBackground source={backgroundImage} style={styles.background}>
+        <Text style={styles.title}>Task</Text>
+        <View style={styles.formContainer}>
+          <Text style={styles.subtitle}>
+            {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+          </Text>
+          {this.state.stageNew && (
+            <TextInput
+              placeholder="Nome"
+              style={styles.input}
+              value={this.state.name}
+              onChangeText={(name) => this.setState({name})}
+            />
+          )}
+          <TextInput
+            placeholder="E-mail"
+            style={styles.input}
+            value={this.state.email}
+            onChangeText={(email) => this.setState({email})}
+          />
+          <TextInput
+            placeholder="Senha"
+            style={styles.input}
+            value={this.state.password}
+            onChangeText={(password) => this.setState({password})}
+          />
+          {this.state.stageNew && (
+            <TextInput
+              placeholder="Confirmação"
+              style={styles.input}
+              value={this.state.confirmPassword}
+              onChangeText={(confirmPassword) =>
+                this.setState({confirmPassword})
+              }
+            />
+          )}
+          <TouchableOpacity onPress={this.signinOrSignup}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>
+                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{padding: 10}}
+            onPress={() => this.setState({stageNew: !this.stageNew})}>
+            <Text style={styles.buttonText}>
+              {this.state.stageNew
+                ? 'Já possui conta?'
+                : 'Ainda não possui conta?'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    );
+  }
 }
