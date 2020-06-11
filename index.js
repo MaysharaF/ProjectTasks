03beeb55b562af4de21c6b47e4142e0c902e82ed
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-06-11 15:58:29 -0300

    tela de autenticação

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
+import Navigator from './src/Navigator';
 import {name as appName} from './app.json';
-import Agenda from './src/screens/Agenda';
 
-AppRegistry.registerComponent(appName, () => Agenda);
+AppRegistry.registerComponent(appName, () => Navigator);
--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {
   StyleSheet,
-  TextComponent,
+  Text,
   TextInput,
   View,
   ImageBackground,
@@ -87,3 +87,43 @@ export default class Auth extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 70,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+  },
+  formContainer: {
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    padding: 20,
+    width: '90%',
+  },
+  input: {
+    marginTop: 10,
+    backgroundColor: '#FFF',
+  },
+  button: {
+    backgroundColor: '#080',
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+  },
+});
